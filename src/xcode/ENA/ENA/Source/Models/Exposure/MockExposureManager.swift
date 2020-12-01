435c0761fe3a14483805fd43ab264cfed755859f
Panos Louridas <louridas@gmail.com>
2020-12-01 11:32:28 +0200

    fix log message

--- src/xcode/ENA/ENA/Source/Models/Exposure/MockExposureManager.swift
+++ src/xcode/ENA/ENA/Source/Models/Exposure/MockExposureManager.swift
@@ -87,7 +87,7 @@ extension MockExposureManager: ExposureManager {
 		notificationCenter.requestAuthorization(options: options) { _, error in
 			if let error = error {
 				// handle error
-				log(message: "Notification authorization request error: \(error.localizedDescription)", level: .error)
+				Log.error("Notification authorization request error: \(error.localizedDescription)", log: .api)
 			}
 			DispatchQueue.main.async {
 				completionHandler()
